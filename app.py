8b1b4f8aa1652584117cfe24b928ca4bfc12ece6
PriyanshuDey23 <priyanshudey.ds@gmail.com>
2025-02-10 21:11:08 +0530

    character network added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 import os
 from Theme_Classifier import ThemeClassifier
+from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 
 # Function to handle save path
 def handle_save_path(save_path):
@@ -44,6 +45,17 @@ def get_themes(theme_list_str, subtitles_path, save_path):
     return output_chart
 
 
+def get_character_network(subtitles_path,ner_path):
+    ner = NamedEntityRecognizer()
+    ner_df = ner.get_ners(subtitles_path,ner_path)
+
+    character_network_generator = CharacterNetworkGenerator()
+    relationship_df = character_network_generator.generate_character_network(ner_df)
+    html = character_network_generator.draw_network_graph(relationship_df)
+
+    return html
+
+
 def main():
     with gr.Blocks() as iface:
         with gr.Row():  # Row
@@ -64,6 +76,21 @@ def main():
                         get_themes_button = gr.Button("Get Themes")
                         # Train and get output
                         get_themes_button.click(get_themes, inputs=[theme_list, subtitles_path, save_path], outputs=[plot])  # Call the function and update plot
+
+
+
+        # Character Network Section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Character Network (NERs and Graphs)</h1>")
+                with gr.Row():
+                    with gr.Column():
+                        network_html = gr.HTML()
+                    with gr.Column():
+                        subtitles_path = gr.Textbox(label="Subtitles or Script Path")
+                        ner_path = gr.Textbox(label="NERs save path")
+                        get_network_graph_button = gr.Button("Get Character Network")
+                        get_network_graph_button.click(get_character_network, inputs=[subtitles_path,ner_path], outputs=[network_html])
 
     # Launch the Gradio interface and allow sharing in Colab
     iface.launch(share=True, inline=False)  # 'inline=False' is important for Colab
